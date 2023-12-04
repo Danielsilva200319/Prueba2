61e5f4f16fec6d27c446734c3bd55d63a8ff5e48
Danielsilva200319 <89894235+Danielsilva200319@users.noreply.github.com>
2023-12-04 16:00:21 -0500

    program

--- Program.cs
+++ Program.cs
@@ -2,6 +2,16 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("      Multiplicación Rusa".ToUpper());
+        Console.Write("Ingrese el número multiplicador: ");
+        int multiplicador = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ingrese el número multiplicando: ");
+        int multiplicando = Convert.ToInt32(Console.ReadLine());
+        int pares = multiplicador % 2;
+        
+        /* int resultado;
+        resultado = 12;
+        Console.WriteLine($"{resultado}");
+        Console.WriteLine($" {multiplicador}, {multiplicando}"); */
     }
 }
